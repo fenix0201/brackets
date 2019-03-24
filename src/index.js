3d5ede7a220055e16626c5be3771ff45cdebc908
unknown <ippk2016@yandex.ru git config --global user.name fenix0201git statutgit config --global user.email ippk2016@yandex.ru>
2019-03-24 20:31:50 +0300

    feat:function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    let found;
+    let same;
+    for(let i in str){
+        found = false;
+        same = false;
+        if(stack.length == 0){
+            stack.push(str[i]);
+            continue;
+        }
+        for(let j in bracketsConfig){
+            if(bracketsConfig[j][1] == str[i]){
+            found = true;
+            let index = stack.length - 1;
+            if(bracketsConfig[j][1] == bracketsConfig[j][0]){
+                same = true;
+            }
+                if(stack[index] == bracketsConfig[j][0]){
+                    stack.pop();
+                    same = false;
+                    break;
+                }
+            }
+        }
+        if(found == false || same == true){
+            stack.push(str[i]);
+        }
+    }
+    if(stack.length == 0)
+        return true;
+    else
+        return false;
 }
